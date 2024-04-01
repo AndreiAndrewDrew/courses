5429ef3e13a1d217b9ef19e0f772f4e8de6f5017
AndreiAndrewDrew <andreiandrewg@gmail.com>
2024-04-01 10:58:25 +0300

    Adaugat Selectarea pe categorii

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views  # '.' importam tat modulul sub numele 'views'
 
+app_name = 'shop'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('courses/', views.courses, name='courses'),
+    path('course_selected/id=<int:course_id>', views.course_selected, name='course_selected'),
+    path('category/', views.category, name='category'),
+    path('category_selected/id=<int:category_id>', views.category_selected, name='category_selected')
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Course  # din mapa curenta din 'models.py' importam variabila 'Course'
+from .models import Category
 
 
 # Create your views here.
-def index(request):
+def courses(request):
     courses = Course.objects.all()
-    return render(request, 'courses.html', {'courses':courses})
+    return render(request, 'shop/courses.html', {'courses': courses})
     # return HttpResponse(courses)
     # return HttpResponse(str(course) + '<br>' for course in courses)
     # return HttpResponse(''.join([str(course) + '<br>' for course in courses]))
+
+
+def course_selected(request, course_id):
+    # Varianta 1
+    # try:
+    #     course = Course.objects.get(pk=course_id)
+    #     return render(request, 'shop/course_selected.html', {'course': course}) # avem acces la variabila 'course'
+    # except Course.DoesNotExist:
+    #     raise Http404()
+
+    # Varianta 2
+    courses = get_object_or_404(Course, pk=course_id)
+    return render(request, 'shop/course_selected.html', {'course': courses})
+
+
+def category(request):
+    category = Category.objects.all()
+    return render(request, 'shop/category.html', {'category': category})
+
+
+def category_selected(request, category_id):
+    # category = Category.objects.get(pk=category_id)
+    courses = Course.objects.filter(category_id=category_id)
+    return render(request, 'shop/category_selected.html', {'courses': courses})
